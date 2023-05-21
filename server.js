0c05f6042e49150d8358bd5018d4ca641dbb7041
rikilega <rosedoland33@yahoo.com>
2023-05-21 15:56:54 -0700

    added delete route to the server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,35 +9,64 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    fs.writeFile(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(notes, null, 2),
+      (err) => {
         if (err) throw err;
-        res.json(JSON.parse(data));
-    });
+        res.json(newNote);
+      }
+    );
+  });
 });
 
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuidv4();
-    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+
+    let notes = JSON.parse(data);
+
+    // Filter out the note with the given ID
+    notes = notes.filter((note) => note.id !== noteId);
+
+    // Write the updated notes to the file
+    fs.writeFile(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(notes, null, 2),
+      (err) => {
         if (err) throw err;
-        const notes = JSON.parse(data);
-        notes.push(newNote);
-        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes, null, 2), (err) => {
-            if (err) throw err;
-            res.json(newNote);
-        });
-    });
+
+        // Send the updated notes as the response
+        res.json(notes);
+      }
+    );
+  });
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
